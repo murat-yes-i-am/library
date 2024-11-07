56e4ed9d612720ae34f6a5513f7861b66be67605
murat-yesimbietov <yesimbietovm@gmail.com>
2024-11-07 10:48:30 +0200

    feat: add info method for Book

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages`;
+}
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
